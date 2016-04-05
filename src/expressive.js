7def27773e45a418f7a6780a66e3830596b82614
Asrul Hanafi <hanafi.asrul@gmail.com>
2016-04-05 09:08:12 +0700

    Remove perfectscrollbar

--- src/expressive.js
+++ src/expressive.js
@@ -3,21 +3,6 @@
 
   // Custom script
   window.onload = function() {
-    var classExvScroll = document.querySelectorAll('.elm-scroll');
-    [].forEach.call(classExvScroll, function(el) {
-      Ps.initialize(el);
-    });
-
-    var contentScroller = document.getElementById("contentScroller");
-    if (contentScroller) {
-      var scope = angular.element(contentScroller).scope();
-      Ps.initialize(contentScroller);
-      scope.$on('$routeChangeStart', function(next, current) {
-        contentScroller.scrollTop = 0;
-        Ps.update(contentScroller);
-      });
-    }
-
     var menuListItem = document.getElementsByClassName('menu-list');
     var eleListItem = angular.element(menuListItem);
     [].forEach.call(eleListItem, function(el) {
